8810a1f669ba13780e109e899182572c0afbf7ec
natal <natal@CLOWN>
2024-05-04 11:47:24 +0400

    begin

--- console/console.cpp
+++ console/console.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <aisd_graph/aisd_graph.h>
 
 using namespace std;
+using namespace Graph_lab;
 
 int main() {
-	cout << "Begin";
+	Graph<int> a;
+	a.add_vertex(5);
+	a.add_vertex(2);
+	a.add_vertex(3);
+	cout<<a.has_vertex(5)<<endl;
+	a.remove_vertex(5);
+
 	return 0;
 }
--- include/aisd_graph/aisd_graph.h
+++ include/aisd_graph/aisd_graph.h
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	cout << "Begin";
-	return 0;
+namespace Graph_lab {
+	template <typename Vertex, typename Distance = double>
+	class Graph {
+		std::unordered_map <Vertex, std::vector<std::pair<Vertex, Distance>>> data;
+	public:
+		bool has_vertex(const Vertex& v) const {
+			if (data.find(v) == data.end()) {
+				return false;
+			}
+			return true;
+		}
+		void add_vertex(const Vertex& v) {
+			data[v] = vector<pair<Vertex, Distance>>();
+		}
+		bool remove_vertex(const Vertex& v) {
+			for (auto& ver : data) {
+				cout << ver.first;
+			}
+			return true;
+		}
+	};
 }
